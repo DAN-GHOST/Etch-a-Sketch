e17c66a4d61077f02b6954278e84d02cd2b8c854
Enrique Fernández <dpuentesf1@ucentral.edu.co>
2023-12-02 15:59:23 -0500

    Se arreglo el tamaño del arreglo creando un arreglo que es capas de adptar su tamaño segun la cantidad de divs y de la pantalla, ademas se configuro un boton y una barra de incremto para aumentar o disminur el tamaño del arreglo. Se crearon estilos para dar una mejor apariencia y se corrigieron errores de logica

--- script.js/logic.js
+++ script.js/logic.js
@@ -1,11 +1,11 @@
-function generarDiv(){
+function generarDiv(tamanioMatriz = 16){
     const divContenedor = document.querySelector('.contenedor-de-divs');
-    for(let i = 0; i<16; i++){
+    for(let i = 0; i<tamanioMatriz; i++){
         const divs = document.createElement('div');
         divs.className = `divs-${i} divsCambiantesColum`;
         divContenedor.appendChild(divs);
         const divCambiante = document.querySelector(`.divs-${i}`);
-        for(let j = 0; j<16; j++){
+        for(let j = 0; j<tamanioMatriz; j++){
             const divs2 = document.createElement('div');
             divs2.className = `divs2-${j} Divs2CambiantesRow`;
             divCambiante.appendChild(divs2);
@@ -13,4 +13,35 @@ function generarDiv(){
     }
 }
 
-window.onload = generarDiv(); // Esto sirve para llamar a la funcion cuando se halla cargado por completo el html, css.
+function limpiarDiv(){
+    let evaluar = false,
+    i = 0;
+    while(evaluar === false){
+        const divCambiante = document.querySelector(`.divs-${i}`);
+        if(divCambiante === null){
+            evaluar = true;
+        }else{
+            ++i;
+            divCambiante.remove();
+        }
+    }
+}
+
+function rangeSlider(value){
+    document.getElementById('rangeValue').innerHTML= `${value}x${value}`;
+}
+
+const getValue = document.querySelector('.rangeValue');
+const btnValorBoton = document.querySelector('.btnEnviarValor');
+btnValorBoton.addEventListener('click',()=>{
+    limpiarDiv();
+    let valor = null;
+    if( getValue.textContent.length === 5){
+        valor = Number(getValue.textContent.substring(0,2));
+    }else{
+        valor = Number(getValue.textContent.substring(0,3));
+    }
+    generarDiv(valor);
+});
+
+window.onload = generarDiv(); // Esto sirve para llamar a la funcion cuando se halla cargado por primera vez el html, css.
