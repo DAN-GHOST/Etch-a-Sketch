6e3d8d6cfa5e80d39dc3def70c0b0f31c3d170a9
Enrique Fernández <dpuentesf1@ucentral.edu.co>
2023-12-05 15:56:43 -0500

    Se realizo toda la logica de funcionamiento con los botones y el cambio de color de la matriz

--- script.js/logic.js
+++ script.js/logic.js
@@ -8,10 +8,13 @@ function generarDiv(tamanioMatriz = 16){
         for(let j = 0; j<tamanioMatriz; j++){
             const divs2 = document.createElement('div');
             divs2.className = `divs2-${j} Divs2CambiantesRow`;
+            divs2.id = `divs-${i}-divs2-${j}`;
             divCambiante.appendChild(divs2);
         }
     }
 }
+
+window.onload = generarDiv();
 
 function limpiarDiv(){
     let evaluar = false,
@@ -27,14 +30,28 @@ function limpiarDiv(){
     }
 }
 
+function limpiarTodo(){
+    const getTypeColor = document.querySelector('.color');
+    getTypeColor.value = '#000000';
+    limpiarDiv();
+    generarDiv();
+}
+
 function rangeSlider(value){
     document.getElementById('rangeValue').innerHTML= `${value}x${value}`;
 }
 
-const getValue = document.querySelector('.rangeValue');
-const btnValorBoton = document.querySelector('.btnEnviarValor');
-btnValorBoton.addEventListener('click',()=>{
+function movimientoDivs(id){
+    if(id !== "1"){
+        const colorBotton = document.querySelector('.btnUsarColor');
+        const divCambiante = document.querySelector("#"+id);
+        divCambiante.style.backgroundColor = colorBotton.value;
+    }
+}
+
+document.querySelector('.btnEnviarValor').addEventListener('click',()=>{
     limpiarDiv();
+    const getValue = document.querySelector('.rangeValue');
     let valor = null;
     if( getValue.textContent.length === 5){
         valor = Number(getValue.textContent.substring(0,2));
@@ -44,4 +61,14 @@ btnValorBoton.addEventListener('click',()=>{
     generarDiv(valor);
 });
 
-window.onload = generarDiv(); // Esto sirve para llamar a la funcion cuando se halla cargado por primera vez el html, css.
+document.querySelector('.btnUsarColor').addEventListener('click', ()=>{
+    const getTypeColor = document.querySelector('.color');
+    const colorBotton = document.querySelector('.btnUsarColor');
+    colorBotton.value = getTypeColor.value;
+});
+
+document.querySelector('.contenedor-de-divs').addEventListener('mousemove', (event)=>{movimientoDivs(event.target.id,event)});
+
+document.querySelector('.btnLiampiarDivs').addEventListener('click',()=>{
+    limpiarTodo();
+});
